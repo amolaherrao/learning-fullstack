d853b0c27d7e0dadae4411e4149dcb3ba9fdbd59
amolaher92 <amolaher92@gmail.com>
2023-09-30 21:54:21 +0530

    updated initial setup

---
+++ python/main.py
@@ -0,0 +1,17 @@
+def greet(msg):
+    print(msg)
+
+greet("Hello Amol")
+
+def my_data_type(param):
+    return "param type", type(param)
+
+result = my_data_type({1,2,3,4,5})
+print(result)
+
+myArr = [1,2,3,4,5]
+for num in myArr:
+    print(num)
+
+
+
